de4f8fd5c39a3a9b2311d6aa263976eb14d2f7ce
oscarnar <orosas58@gmail.com>
2021-12-13 18:57:50 -0500

    First structure

---
+++ lib/main_production.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2021, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:mi_libro_vecino/app/app.dart';
+import 'package:mi_libro_vecino/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
