03343f754e6706a4031a3733538a078a79afc807
oscarnar <orosas58@gmail.com>
2021-12-15 17:57:58 -0500

    Typography has been added

---
+++ lib/utils_ui/typography.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:mi_libro_vecino/utils_ui/colors.dart';
+
+abstract class PFontWeight {
+  /// FontWeight value of `w900`
+  static const FontWeight black = FontWeight.w900;
+
+  /// FontWeight value of `w800`
+  static const FontWeight extraBold = FontWeight.w800;
+
+  /// FontWeight value of `w700`
+  static const FontWeight bold = FontWeight.w700;
+
+  /// FontWeight value of `w600`
+  static const FontWeight semiBold = FontWeight.w600;
+
+  /// FontWeight value of `w500`
+  static const FontWeight medium = FontWeight.w500;
+
+  /// FontWeight value of `w400`
+  static const FontWeight regular = FontWeight.w400;
+
+  /// FontWeight value of `w300`
+  static const FontWeight light = FontWeight.w300;
+
+  /// FontWeight value of `w200`
+  static const FontWeight extraLight = FontWeight.w200;
+
+  /// FontWeight value of `w100`
+  static const FontWeight thin = FontWeight.w100;
+}
+
+class PTextStyle {
+  static const _baseTextStyle = TextStyle(
+    package: 'mi_libro_vecino',
+    fontFamily: 'NotoSans',
+    color: PColors.black,
+    fontWeight: PFontWeight.regular,
+  );
+
+  /// Headline 1 Text Style
+  static TextStyle get headline1 {
+    return _baseTextStyle.copyWith(
+      fontSize: 36,
+      fontWeight: PFontWeight.bold,
+    );
+  }
+
+  /// Headline 2 Text Style
+  static TextStyle get headline2 {
+    return _baseTextStyle.copyWith(
+      fontSize: 24,
+      fontWeight: PFontWeight.semiBold,
+    );
+  }
+
+  /// Headline 3 Text Style
+  static TextStyle get headline3 {
+    return _baseTextStyle.copyWith(
+      fontSize: 24,
+      fontWeight: PFontWeight.regular,
+    );
+  }
+
+  /// Headline 4 Text Style
+  static TextStyle get headline4 {
+    return _baseTextStyle.copyWith(
+      fontSize: 20,
+      fontWeight: PFontWeight.regular,
+    );
+  }
+
+  /// Headline 5 Text Style not found
+
+  /// Headline 6 Text Style not found
+
+  /// Subtitle 1 Text Style
+  static TextStyle get subtitle1 {
+    return _baseTextStyle.copyWith(
+      fontSize: 16,
+      fontWeight: PFontWeight.regular,
+    );
+  }
+
+  /// Subtitle 2 Text Style
+  static TextStyle get subtitle2 {
+    return _baseTextStyle.copyWith(
+      fontSize: 16,
+      fontWeight: PFontWeight.bold,
+    );
+  }
+
+  /// Body Text 1 Text Style
+  static TextStyle get bodyText1 {
+    return _baseTextStyle.copyWith(
+      fontSize: 16,
+      fontWeight: PFontWeight.regular,
+    );
+  }
+
+  /// Body Text 2 Text Style (the default)
+  static TextStyle get bodyText2 {
+    return _baseTextStyle.copyWith(
+      fontSize: 14,
+      fontWeight: PFontWeight.regular,
+    );
+  }
+
+  /// Caption Text Style
+  static TextStyle get caption {
+    return _baseTextStyle.copyWith(
+      fontSize: 12,
+      fontWeight: PFontWeight.regular,
+    );
+  }
+
+  /// Overline Text Style not found
+
+  /// Button Text Style
+  static TextStyle get button {
+    return _baseTextStyle.copyWith(
+      fontSize: 16,
+      fontWeight: PFontWeight.semiBold,
+    );
+  }
+}
