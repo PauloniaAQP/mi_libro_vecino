df08d197a3654439cff395a1cc7623b763311d65
oscarnar <orosas58@gmail.com>
2021-12-16 15:42:34 -0500

    Colors has been added

---
+++ lib/ui_utils/colors.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+abstract class PColors {
+  /// Black
+  static const Color black = Color(0xFF333333);
+
+  /// Gray 1
+  static const Color gray1 = Color(0xFF5D5D5D);
+
+  /// Gray 2
+  static const Color gray2 = Color(0xFF4F4F4F);
+
+  /// Gray 3
+  static const Color gray3 = Color(0xFFF4F4F4);
+
+  /// Gray 4
+  static const Color gray4 = Color(0xFFAEAEAE);
+
+  /// Purple
+  static const Color purple = Color(0xFF24204A);
+
+  /// White
+  static const Color white = Color(0xFFFFFFFF);
+
+  /// WhiteBackground
+  static const Color whiteBackground = Color(0xFFF7F7FA);
+
+  /// Transparent
+  static const Color transparent = Color(0x00000000);
+
+  /// Blue
+  static const Color blue = Color(0xFF2183DF);
+
+  /// Blue Variant
+  static const Color blueVariant = Color(0xFF1282EC);
+
+  /// Red
+  static const Color red = Color(0xFFE92D27);
+
+  /// Green
+  static const Color green = Color(0xFF2EAD86);
+
+  /// Orange
+  static const Color orange = Color(0xFFFB7F0E);
+}
