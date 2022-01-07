7ed564d05e708a5cb8f1f0cfd6c9656bc29dd5be
oscarnar <orosas58@gmail.com>
2022-01-07 10:03:22 -0500

    Search widget has been added

--- lib/libraries/view/libraries_page.dart
+++ lib/libraries/view/libraries_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mi_libro_vecino/l10n/l10n.dart';
 import 'package:mi_libro_vecino/libraries/components/libraries_appbar.dart';
 import 'package:mi_libro_vecino/search/widgets/search_widget.dart';
+import 'package:mi_libro_vecino/ui_utils/constans/assets.dart';
 
 class LibrariesPage extends StatelessWidget {
   const LibrariesPage({Key? key}) : super(key: key);
@@ -9,6 +10,7 @@ class LibrariesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
+    const imgPath = 'assets/images/library.jpeg';
     return Scaffold(
       appBar: const LibrariesAppBar(),
       body: Row(
@@ -19,12 +21,39 @@ class LibrariesPage extends StatelessWidget {
                 const SizedBox(height: 16),
                 Column(
                   children: [
-                    const SizedBox(height: 16),
+                    const SizedBox(height: 100),
                     Text('test'),
+                    FlutterLogo(
+                      size: 200,
+                    ),
+                    Container(
+                      height: 163,
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: Row(
+                        children: [
+                          Container( /// CUBIT AND STATE FOR LIBRERY LIST AND INFORMATION LIBRARY
+                            width: 128,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(14),
+                              image: DecorationImage(
+                                image: AssetImage(imgPath),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          Column(
+                            children: [
+                              Text('aasdasdasd'),
+                              Text('aasdasdasd'),
+                              Chip(label: Text('asdasdasd')),
+                            ],
+                          )
+                        ],
+                      ),
+                    )
                   ],
                 ),
                 SearchWidget(),
-                FlutterLogo(size: 200,),
               ],
             ),
           ),
