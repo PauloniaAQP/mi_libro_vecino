5fa1262422f064d7731f7a4f5fb027d11a9e9e7d
oscarnar <orosas58@gmail.com>
2022-01-16 14:27:09 -0500

    Test for personal photo page has been created

---
+++ test/register/view/pages/personal_photo_page_test.dart
@@ -0,0 +1,38 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mi_libro_vecino/register/cubit/register_cubit.dart';
+import 'package:mi_libro_vecino/register/view/register_page.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../helpers/pump_app.dart';
+
+class MockSearchCubit extends MockCubit<RegisterState>
+    implements RegisterCubit {}
+
+void main() {
+  group('Register personal photo page', () {
+    late RegisterCubit registerCubit;
+
+    setUp(() {
+      registerCubit = MockSearchCubit();
+    });
+
+    testWidgets('renders Register personal photo Page', (tester) async {
+      final state = RegisterInitial();
+      when(() => registerCubit.state).thenReturn(state.copyWith(index: 2));
+      await tester.pumpApp(
+        BlocProvider.value(
+          value: registerCubit,
+          child: const RegisterPage(),
+        ),
+      );
+      expect(
+        find.text(
+          'SUBIR UNA FOTO',
+        ),
+        findsOneWidget,
+      );
+    });
+  });
+}
