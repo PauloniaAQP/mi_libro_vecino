5b463f82f2f59a66f3df5f87d4994aba895c0333
oscarnar <orosas58@gmail.com>
2022-01-16 13:03:27 -0500

    Test for register has been updated

--- test/register/view/pages/register_error_page_test.dart
+++ test/register/view/pages/register_error_page_test.dart
@@ -1,35 +1,36 @@
 import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mi_libro_vecino/register/cubit/register_cubit.dart';
-import 'package:mi_libro_vecino/register/view/register_page.dart';
+import 'package:mi_libro_vecino/register/view/pages/register_error_page.dart';
 import 'package:mocktail/mocktail.dart';
 
-import '../../helpers/pump_app.dart';
+import '../../../helpers/pump_app.dart';
 
 class MockSearchCubit extends MockCubit<RegisterState>
     implements RegisterCubit {}
 
 void main() {
-  group('Register page', () {
+  group('Register error page', () {
     late RegisterCubit registerCubit;
 
     setUp(() {
       registerCubit = MockSearchCubit();
     });
 
-    testWidgets('renders Register Page', (tester) async {
+    testWidgets('renders register error Page', (tester) async {
       final state = RegisterInitial();
-      when(() => registerCubit.state).thenReturn(state);
+      when(() => registerCubit.state).thenReturn(state.copyWith(index: 7));
       await tester.pumpApp(
         BlocProvider.value(
           value: registerCubit,
-          child: const RegisterPage(),
+          child: const RegisterErrorPage(),
         ),
       );
       expect(
-        find.text(
-          '@BibliotecaDeLaConfianza',
+        find.byKey(
+          const Key('register_error_page_button'),
         ),
         findsOneWidget,
       );
