4e762a15e8e9cb3eaef3670ac49e251fd5ae80e7
oscarnar <orosas58@gmail.com>
2022-01-16 12:54:41 -0500

    Tests for libraries module have been created

---
+++ test/libraries/components/libraries_appbar_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mi_libro_vecino/libraries/components/libraries_appbar.dart';
+
+import '../../helpers/pump_app.dart';
+
+void main() {
+  group('Library app bar widget', () {
+    setUp(() {});
+
+    testWidgets('renders Library app bar widget', (tester) async {
+      await tester.pumpApp(
+        const LibrariesAppBar(),
+      );
+      expect(
+        find.text(
+          'Siguenos',
+        ),
+        findsOneWidget,
+      );
+    });
+  });
+}
--- test/libraries/view/libraries_page_test.dart
+++ test/libraries/view/libraries_page_test.dart
@@ -1,38 +1,45 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mi_libro_vecino/register/cubit/register_cubit.dart';
-import 'package:mi_libro_vecino/register/view/register_page.dart';
+import 'package:mi_libro_vecino/libraries/cubit/libraries_cubit.dart';
+import 'package:mi_libro_vecino/libraries/view/libraries_page.dart';
+import 'package:mi_libro_vecino/search/cubit/search_cubit.dart';
 import 'package:mocktail/mocktail.dart';
 
 import '../../helpers/pump_app.dart';
 
-class MockSearchCubit extends MockCubit<RegisterState>
-    implements RegisterCubit {}
+class MockLibrariesCubit extends MockCubit<LibrariesState>
+    implements LibrariesCubit {}
 
+class MockSearchCubit extends MockCubit<SearchState> implements SearchCubit {}
 
-// TODO(oscarnar): Add tests for the rest of the widgets
 void main() {
   group('Libraries list page', () {
-    late RegisterCubit registerCubit;
+    late LibrariesCubit librariesCubit;
+    late SearchCubit searchCubit;
 
     setUp(() {
-      registerCubit = MockSearchCubit();
+      librariesCubit = MockLibrariesCubit();
+      searchCubit = MockSearchCubit();
     });
 
     testWidgets('renders libraries list Page', (tester) async {
-      final state = RegisterInitial();
-      // when(() => registerCubit.state).thenReturn(state);
+      const state = LibrariesInitial();
+      const searchState = SearchInitial();
+      when(() => librariesCubit.state).thenReturn(state);
+      when(() => searchCubit.state).thenReturn(searchState);
       await tester.pumpApp(
-        // BlocProvider.value(
-        //   value: registerCubit,
-        //   child: const RegisterPage(),
-        // ),
-        const RegisterPage(),
+        BlocProvider.value(
+          value: searchCubit,
+          child: BlocProvider.value(
+            value: librariesCubit,
+            child: const LibrariesPage(),
+          ),
+        ),
       );
       expect(
         find.text(
-          'test',
+          'Soy colaborador',
         ),
         findsOneWidget,
       );
