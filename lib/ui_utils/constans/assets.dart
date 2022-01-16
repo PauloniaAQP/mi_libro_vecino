244f24ede31ab8d26657485e72f1acece00e2e96
oscarnar <orosas58@gmail.com>
2022-01-16 12:55:10 -0500

    New assets have been added

--- lib/ui_utils/constans/assets.dart
+++ lib/ui_utils/constans/assets.dart
@@ -1,5 +1,6 @@
 class Assets {
   static const String logo = 'assets/images/logo.png';
+  static const String logoWithName = 'assets/images/logo_with_name.png';
   static const String backIcon = 'assets/icons/back.png';
   static const String nextIcon = 'assets/icons/next.png';
   static const String profileIcon = 'assets/icons/profile.png';
@@ -7,4 +8,16 @@ class Assets {
   static const String imageIcon = 'assets/icons/image.png';
   static const String imageWhiteIcon = 'assets/icons/image_white.png';
   static const String waitingIcon = 'assets/icons/waiting.png';
+  static const String locationIcon = 'assets/icons/location.png';
+  static const String locationPinIcon = 'assets/icons/location_pin.png';
+  static const String handIcon = 'assets/icons/hand.png';
+  static const String phoneIcon = 'assets/icons/phone.png';
+  static const String facebookLogo = 'assets/images/facebook.png';
+  static const String waitingImg = 'assets/images/waiting_book.png';
+  static const String errorRegisterImg = 'assets/images/register_error_book.png';
+  static const String searchImg = 'assets/images/search_book.png';
+
+  // Image for tests
+  static const testImg = 'assets/images/test_images/library.jpeg';
+  static const registerTestImg = 'assets/images/test_images/register_image.png';
 }
