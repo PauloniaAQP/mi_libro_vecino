0b95c777367cef88e4fdafd2ea4e3bcfca327c49
oscarnar <orosas58@gmail.com>
2022-01-21 19:00:58 -0500

    Searching function has been added

--- lib/admin/cubit/admin_state.dart
+++ lib/admin/cubit/admin_state.dart
@@ -1,10 +1,28 @@
 part of 'admin_cubit.dart';
 
-abstract class AdminState extends Equatable {
-  const AdminState();
+class AdminState extends Equatable {
+  const AdminState({
+    this.isSearching = false,
+    required this.textEditControler,
+  });
+
+  final bool isSearching;
+  final TextEditingController textEditControler;
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [isSearching];
+
+  AdminState copyWith({
+    bool? isSearching,
+  }) {
+    return AdminState(
+      isSearching: isSearching ?? this.isSearching,
+      textEditControler: textEditControler,
+    );
+  }
+
 }
 
-class AdminInitial extends AdminState {}
+class AdminInitial extends AdminState {
+  AdminInitial() : super(textEditControler: TextEditingController());
+}
