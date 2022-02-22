781b0f41d78e1211cb3a97c3a2095c0c12ad3bae
oscarnar <orosas58@gmail.com>
2022-02-21 22:18:24 -0500

    Temporal changes to pass test with Flutter 2.8.1

--- lib/ui_utils/theme.dart
+++ lib/ui_utils/theme.dart
@@ -21,9 +21,9 @@ class PTheme {
   static ColorScheme get _colorScheme {
     return const ColorScheme(
       primary: PColors.blue,
-      primaryContainer: PColors.blue,
+      primaryVariant: PColors.blue,
       secondary: PColors.blue,
-      secondaryContainer: PColors.blueVariant,
+      secondaryVariant: PColors.blueVariant,
       surface: PColors.whiteBackground,
       background: PColors.whiteBackground,
       error: PColors.red,
