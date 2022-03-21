0816ea7e52ad18499eac0497c9e624fec45fd41a
oscarnar <orosas58@gmail.com>
2022-03-20 23:49:12 -0500

    Some utils functions have been added

--- lib/ui_utils/functions.dart
+++ lib/ui_utils/functions.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
 
+import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:mi_libro_vecino/l10n/l10n.dart';
+import 'package:mi_libro_vecino_api/utils/constants/enums/library_enums.dart';
 
 Future<File?> uiPickImage({
   ImageSource? imageSource,
@@ -21,4 +24,27 @@ Future<File?> uiPickImage({
     }
   }
   return image;
+}
+
+TimeOfDay fromStringToTimeOfDay(String time) {
+  final timeSplit = time.split(':');
+  return TimeOfDay(
+    hour: int.parse(timeSplit[0]),
+    minute: int.parse(timeSplit[1]),
+  );
+}
+
+/// Gets the string of library type from the enum
+/// [l10n] could be get from context.l10n
+String getStringRolByType(LibraryType type, AppLocalizations l10n) {
+  switch (type) {
+    case LibraryType.bookshop:
+      return l10n.registerPageRolBookshop;
+    case LibraryType.mediator:
+      return l10n.registerPageRolMediator;
+    case LibraryType.editorial:
+      return l10n.registerPageRolEditorial;
+    case LibraryType.library:
+      return l10n.registerPageRolLibrary;
+  }
 }
