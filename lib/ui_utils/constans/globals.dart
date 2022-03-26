e28c99bb5b6e4723a937564a2313fb933eb5efb7
oscarnar <orosas58@gmail.com>
2022-03-26 00:23:50 -0500

    Regex expresion for websites has been added

--- lib/ui_utils/constans/globals.dart
+++ lib/ui_utils/constans/globals.dart
@@ -1,3 +1,5 @@
 class Globals {
   static const String pTimeRegex = r'^(0?[1-9]|1[0-2]):[0-5][0-9]$';
+  static const String pWebsiteRegex =
+      r'^(http\:\/\/|https\:\/\/)?([a-z0-9][a-z0-9\-]*\.)+[a-z0-9][a-z0-9\-]*$';
 }
