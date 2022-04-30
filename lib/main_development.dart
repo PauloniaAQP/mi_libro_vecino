c103a258a6c8d442642f43a7de7995e51a4262f7
oscarnar <orosas58@gmail.com>
2022-04-29 19:12:48 -0500

    Catcher has been added

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -7,22 +7,33 @@
 
 import 'dart:async';
 import 'package:bloc/bloc.dart';
+import 'package:catcher/catcher.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:mi_libro_vecino/app/app.dart';
 import 'package:mi_libro_vecino/bloc_observer.dart';
 import 'package:mi_libro_vecino/bootstrap.dart';
+import 'package:paulonia_error_service/paulonia_error_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  unawaited(
-    bootstrap(
-      () => BlocOverrides.runZoned(
-        () => const App(),
-        blocObserver: MyLibroVecinoBlocObserver(),
-      ),
-    ),
+  final catcherConf = PauloniaErrorService.getCatcherConfig();
+
+  Catcher(
+    runAppFunction: () {
+      unawaited(
+        bootstrap(
+          () => BlocOverrides.runZoned(
+            () => const App(),
+            blocObserver: MyLibroVecinoBlocObserver(),
+          ),
+        ),
+      );
+    },
+    debugConfig: catcherConf['debug'],
+    releaseConfig: catcherConf['release'],
+    profileConfig: catcherConf['profile'],
   );
 }
