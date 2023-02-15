267fdfc95c80f5f07403ae69a044fe586f2bcb72
GonzaloCaira <gcairaaguilar@gmail.com>
2023-02-15 12:13:51 -0500

    fix flutter analyze lib

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -23,16 +23,11 @@ Future<void> main() async {
   await Firebase.initializeApp();
 
   final catcherConf = PauloniaErrorService.getCatcherConfig();
-
+  Bloc.observer = MyLibroVecinoBlocObserver();
   Catcher(
     runAppFunction: () {
       unawaited(
-        bootstrap(
-          () => BlocOverrides.runZoned(
-            () => const App(),
-            blocObserver: MyLibroVecinoBlocObserver(),
-          ),
-        ),
+        bootstrap(() => const App()),
       );
     },
     debugConfig: catcherConf['debug'],
